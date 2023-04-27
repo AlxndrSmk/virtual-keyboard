441d7e6484e861bf00fa6723950d86826748597f
Alexander Samak <voprosys262@gmail.com>
2023-04-27 22:34:00 +0300

    refactor: add ES6+ class feature

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,39 @@
-import createDOM from './components/createDOM.js';
+import fragment from './components/keyboardRows.js';
 
-window.addEventListener('load', () => {
-  createDOM();
+class Keyboard {
+  constructor() {
+    this.capsLock = false;
+    this.language = localStorage.getItem('lang') === 'ru' ? 'ru' : 'en';
+  }
+
+  init() {
+    this.title = document.createElement('h1');
+    this.textarea = document.createElement('textarea');
+    this.keyboard = document.createElement('div');
+    this.description = document.createElement('p');
+    this.language = document.createElement('p');
+
+    this.title.innerText = 'RSS Виртуальная клавиатура';
+    this.description.innerText = 'Клавиатура создана в операционной системе MacOS';
+    this.language.innerText = 'Для переключения языка: левыe ctrl + alt (Windows), левые control + option (MacOS)';
+
+    this.textarea.autofocus = true;
+    this.title.classList.add('title');
+    this.textarea.classList.add('textarea');
+    this.keyboard.classList.add('keyboard');
+    this.description.classList.add('description');
+    this.language.classList.add('description');
+
+    document.body.append(this.title);
+    document.body.append(this.textarea);
+    document.body.append(this.keyboard);
+    this.keyboard.append(fragment);
+    document.body.append(this.description);
+    document.body.append(this.language);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const keyboard = new Keyboard();
+  keyboard.init();
 });
