6008592e05bd984380aef0cb2fad51310ae1e7fb
Alexander Samak <voprosys262@gmail.com>
2023-04-27 21:45:06 +0300

    add all keyboard buttons

--- src/js/components/keyboardRows.js
+++ src/js/components/keyboardRows.js
@@ -12,6 +12,19 @@ keyboardButtons.forEach((row) => {
     keyboardButton.classList.add(`btn_size-${btn.size}`);
     keyboardButton.textContent = btn.lang.en;
 
+    if (btn.name === 'ArrowRight') {
+      const arrowWrapper = document.createElement('div');
+      arrowWrapper.classList.add('arrow-buttons-wrapper');
+
+      arrowWrapper.innerHTML = `
+      <button class="btn btn_size-xs">▴</button>
+      <button class="btn btn_size-xs">▾</button>
+      `;
+      keyboardRow.appendChild(arrowWrapper);
+    }
+
+    if (btn.size === 'xs') return;
+
     keyboardRow.appendChild(keyboardButton);
   });
 
